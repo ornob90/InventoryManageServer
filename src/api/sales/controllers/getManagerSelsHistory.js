8505f87ea8d81250faf4b3a71a78397d0cc76f61
ornob90 <oornob49@gmail.com>
2023-11-24 16:15:29 +0600

    get sales history api complete

---
+++ src/api/sales/controllers/getManagerSelsHistory.js
@@ -0,0 +1,42 @@
+const Sales = require("../../../models/sales");
+
+const getManagerSalesHistory = async (req, res, next) => {
+  try {
+    const { email } = req.query;
+
+    const pipeline = [
+      {
+        $lookup: {
+          from: "products",
+          localField: "product",
+          foreignField: "_id",
+          as: "productInfo",
+        },
+      },
+      {
+        $unwind: "$productInfo",
+      },
+      {
+        $match: {
+          "productInfo.userEmail": email,
+        },
+      },
+      {
+        $project: {
+          productName: "$productInfo.userEmail",
+          profit: 1,
+          sellingDate: 1,
+        },
+      },
+    ];
+
+    const salesHistory = await Sales.aggregate(pipeline);
+
+    res.send(salesHistory);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+module.exports = getManagerSalesHistory;
--- src/routes/sales/index.js
+++ src/routes/sales/index.js
@@ -1,10 +1,12 @@
 const addSale = require("../../api/sales/controllers/addSale");
 const getManagerSaleSummary = require("../../api/sales/controllers/getManagerSaleSummary");
+const getManagerSalesHistory = require("../../api/sales/controllers/getManagerSelsHistory");
 
 const router = require("express").Router();
 
 // Get
 router.get("/manager-sales-summary", getManagerSaleSummary);
+router.get("/manager-sales-history", getManagerSalesHistory);
 
 // POST
 router.post("/sales", addSale);
