598209915bfc92119b2a5927549b21108f632c4b
ornob90 <oornob49@gmail.com>
2023-11-24 18:55:43 +0600

    verify shop Admin complete

---
+++ src/middlewares/verifyShopAdmin.js
@@ -0,0 +1,20 @@
+const User = require("../models/user");
+
+const verifyShopAdmin = async (req, res, next) => {
+  try {
+    const { email } = req.decoded;
+
+    const { role } = await User.findOne({ email });
+    console.log(role);
+    if (!role || role !== "shopAdmin") {
+      res.status(403).send({ message: "unauthorized access" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+module.exports = verifyShopAdmin;
--- src/routes/shop/index.js
+++ src/routes/shop/index.js
@@ -1,11 +1,12 @@
 const addShop = require("../../api/shop/controllers/addShop");
 const getAllShops = require("../../api/shop/controllers/getAllShops");
+const verifyShopAdmin = require("../../middlewares/verifyShopAdmin");
 const verifyToken = require("../../middlewares/verifyToken");
 
 const router = require("express").Router();
 
 // GET
-router.get("/shops", verifyToken, getAllShops);
+router.get("/shops", getAllShops);
 
 // POST
 router.post("/shop", addShop);
