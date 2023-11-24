e966d12140b9d8e532ba6a7320f9fb2bf4fdbdd5
ornob90 <oornob49@gmail.com>
2023-11-24 10:50:02 +0600

    product limit update after delete product

--- src/api/product/controllers/deleteProduct.js
+++ src/api/product/controllers/deleteProduct.js
@@ -1,11 +1,21 @@
 const { ObjectId } = require("mongodb");
 const Product = require("../../../models/product");
+const getShopInfo = require("../../../lib/shop/getShopInfo");
+const updateShopInfo = require("../../../lib/shop/updateShopInfo");
 
 const deleteProduct = async (req, res, next) => {
   try {
     const { id } = req.params;
 
+    const { shopId } = req.body;
+
     const result = await Product.deleteOne({ _id: new ObjectId(id) });
+
+    const shop = await getShopInfo(shopId);
+
+    await updateShopInfo(shopId, {
+      productLimit: shop.productLimit + 1,
+    });
 
     res.send({
       message: "Deleted Successfully",
