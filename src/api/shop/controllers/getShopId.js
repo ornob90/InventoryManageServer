f47f6d995498dd02ec55db7d44be2f2bbda72072
ornob90 <oornob49@gmail.com>
2023-11-26 10:45:31 +0600

    get shop Id api

---
+++ src/api/shop/controllers/getShopId.js
@@ -0,0 +1,15 @@
+const Shop = require("../../../models/shop");
+
+const getShopID = async (req, res, next) => {
+  try {
+    const { email } = req.query;
+
+    const shopID = await Shop.findOne({ shopOwnerEmail: email }, { _id: 1 });
+    res.send({ shopID: shopID._id });
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
+};
+
+module.exports = getShopID;
--- src/routes/shop/index.js
+++ src/routes/shop/index.js
@@ -1,5 +1,6 @@
 const addShop = require("../../api/shop/controllers/addShop");
 const getAllShops = require("../../api/shop/controllers/getAllShops");
+const getShopID = require("../../api/shop/controllers/getShopId");
 const verifyShopAdmin = require("../../middlewares/verifyShopAdmin");
 const verifyToken = require("../../middlewares/verifyToken");
 
@@ -7,6 +8,7 @@ const router = require("express").Router();
 
 // GET
 router.get("/shops", getAllShops);
+router.get("/shopID", getShopID);
 
 // POST
 router.post("/shop", addShop);
